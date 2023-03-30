c367de357c9319b337eac5524ebf71bd209ca3bf
bridgetisvirtuallyhere <bridgetjacosta@gmail.com>
2023-03-30 13:17:14 -0700

    now returns if user is 21 or not

--- main/java/IDScanner/QRCode.java
+++ main/java/IDScanner/QRCode.java
@@ -3,26 +3,21 @@ package IDScanner;
 // Java code to read the QR code
 
 
-import java.io.File;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import javax.imageio.ImageIO;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import javax.imageio.ImageIO;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 
 public class QRCode {
@@ -45,12 +40,19 @@ public class QRCode {
         return result.getText();
     }
 
+    // Function to check if the person is over 21 years old
+    public static boolean isOver21(Date birthDate) {
+        Calendar birthCalendar = Calendar.getInstance();
+        birthCalendar.setTime(birthDate);
+        Calendar nowCalendar = Calendar.getInstance();
+        nowCalendar.add(Calendar.YEAR, -21);
+        return nowCalendar.after(birthCalendar);
+    }
 
-    // Driver code
+        // Driver code
     public static void main(String[] args)
             throws WriterException, IOException,
-            NotFoundException
-    {
+            NotFoundException {
 
 
         // Path where the QR code is saved
@@ -66,9 +68,15 @@ public class QRCode {
                 ErrorCorrectionLevel>();
 
 
-
-
         hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        String dob = getDOBFromQR(path, charset, hashMap);
+        Date birthDate = parseDate(dob);
+
+        if (isOver21(birthDate)) {
+            System.out.println("The person is over 21 years old");
+        } else {
+            System.out.println("The person is under 21 years old");
+        }
 
 
         System.out.println("QRCode output: " + readQR(path, charset, hashMap));
@@ -80,14 +88,22 @@ public class QRCode {
     public static String getDOBFromQR(String path, String charset, Map hashMap)
             throws FileNotFoundException, IOException, NotFoundException {BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(path)))));
 
-
         Result result = new MultiFormatReader().decode(binaryBitmap);
-
 
         String qrText = result.getText();
         String dob = qrText.substring(qrText.indexOf("DBB") + 3, qrText.indexOf("DBB") + 11);
 
-
         return dob;
     }
+
+    private static Date parseDate(String dateString) {
+        try {
+            return new SimpleDateFormat("yyyyMMdd").parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
