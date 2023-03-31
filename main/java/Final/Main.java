febb79b31ff240cedfde18f370a96654b97a4185
Skyler Home Desktop <shunsinger643@gmail.com>
2023-03-31 01:14:05 -0700

    problems with libraries again. need to find a solution to this as everyone keeps running into it. Reminder to self, do NOT add the EmailConfig.json file to the repo

--- main/java/Final/Main.java
+++ main/java/Final/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         //taken from the main in gui demo, has been deleted
         //Application.launch(UserRegistrationPage.class);
+        //MasterFacade.sendEmail("shunsinger643@gmail.com", "subject", "the body of the message");
 
 
     }
